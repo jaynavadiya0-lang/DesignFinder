07fcb5d96071f12352f2c4f481285ce5cc0f9f11
jaynavadiya0-lang <jaynavadiya0@gmail.com>
2026-07-03 13:48:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -436,7 +436,8 @@ def compare_single_feature_patch(f1, f2):
             bf_akaze,
             ratio=0.78
         )
-
+         akaze_score = 0
+        
     hist_score = hist_similarity_score(
         f1["hist"],
         f2["hist"]
